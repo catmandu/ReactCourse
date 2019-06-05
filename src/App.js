102ee1604702c831fd5c5c6006888a794a648cfd
Juan Carlos Uriarte Manzano <juanuri-c@herbalife.com>
2019-06-05 17:44:37 -0500

    conditionals example

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className='App'>
-      <h1>Hello from React</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    const name = 'John Doe';
+    const loading = false;
+    const showName = false;
+
+    return (
+      <div className='App'>
+        <h1>My app</h1>
+        {loading ? <h4>Loading...</h4> : <h1>Hello {showName && name}</h1>}
+      </div>
+    );
+  }
 }
 
 export default App;
